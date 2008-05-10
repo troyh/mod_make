b7cf36544e3e31225e6809150234f574c4aa7002
troy <troy@d72ef72b-16af-4b6c-a44a-6013b1e7d5a2>
2008-05-10 22:44:38 +0000

    Uses regex to determine if the make output is acceptable as an error, i.e., "No rule to make target"

--- mod_make.c
+++ mod_make.c
@@ -2,23 +2,23 @@
 #include <http_protocol.h>
 #include <http_config.h>
 #include <apache2/http_log.h>
+#include <regex.h>
+#include <apr_strings.h>
 
 // Configurable options
-const char* DOCUMENT_ROOT="/var/www/";
-const char* SOURCE_ROOT="/home/troy/app/"; // MakeSourceRoot
-const char* MAKEFILE_NAME="/Makefile"; // MakeFilename
+const char* DOCUMENT_ROOT="/var/www";
+const char* SOURCE_ROOT="/home/troy/app"; // MakeSourceRoot
+const char* MAKEFILE_NAME="Makefile"; // MakeFilename
+const char* MAKE_PROG="make"; // MakeProgram
 const char* MAKE_OPTIONS=""; // MakeOptions
 const char* INCLUDE_FILE_TYPES=""; // MakeIncludeFileTypes
 const char* EXCLUDE_FILE_TYPES=""; // MakeExcludeFileTypes
 const char* EXCLUDE_REGEX=""; // MakeExcludeRegex
-const char* MAKEERROR_URI="/mod_make.html"; // MakeErrorURI
+const char* MAKEERROR_URI="/mod_make_error"; // MakeErrorURI
 
 /**
 TODO:
 - Make above params configurable
-- Make sure MAKEFILE_NAME has a leading slash
-- Make sure SOURCE_ROOT has a trailing slash
-- Make sure DOCUMENT_ROOT has a trailing slash
 - Allow a config option to specify file types to include or exclude (so it doesn't try to make .gif files, for example)
 - Handle non-existent make targets gracefully. We have to run make, but if it says something like the following, silently fail:
 
@@ -28,36 +28,7 @@ TODO:
 
 */
 
-const size_t MAX_PATH=255;
-
-static int printitem(void *rec, const char *key, const char *value)
-{
-    /* rec is a user data pointer.  We'll pass the request_rec in it. */
-    request_rec *r = rec;
-    ap_rprintf(r, "<tr><th scope=\"row\">%s</th><td>%s</td></tr>\n",
-           ap_escape_html(r->pool, key),
-           ap_escape_html(r->pool, value));
-    /* Zero would stop iterating; any other return value continues */
-    return 1;
-}
-
-static void printtable(request_rec *r, apr_table_t *t,
-                 const char *caption, const char *keyhead,
-                 const char *valhead)
-{
-    /* Print a table header */
-    ap_rprintf(r, "<table><caption>%s</caption><thead>"
-           "<tr><th scope=\"col\">%s</th><th scope=\"col\">%s"
-           "</th></tr></thead><tbody>", caption, keyhead, valhead);
-
-    /* Print the data: apr_table_do iterates over entries with
-     * our callback
-     */
-    apr_table_do(printitem, r, t, NULL);
-
-    /* Finish the table */
-    ap_rputs("</tbody></table>\n", r);
-}
+const size_t MAX_PATH=255; // TODO: use OS' equivalent
 
 /**
  *	Handles the error page, when necessary.
@@ -74,30 +45,10 @@ static int make_handler(request_rec *r) {
     ap_rputs("<html><head><title>mod_make</title></head>", r);
     ap_rputs("<body><h1>Make Error:</h1>", r);
 
-	ap_log_rerror(APLOG_MARK,APLOG_ERR,0,r,"mod_make handler:notes table is empty:%s",apr_is_empty_table(r->notes)?"TRUE":"FALSE");
-
 	const char* make_output=apr_table_get(r->prev->notes,"make_output");
-	ap_log_rerror(APLOG_MARK,APLOG_ERR,0,r,"mod_make handler:make_output:%s",make_output);
+	// ap_log_rerror(APLOG_MARK,APLOG_ERR,0,r,"mod_make handler:make_output:%s",make_output);
 	ap_rprintf(r,"<pre>%s</pre>",make_output);
 
-	// ap_rprintf(r,"<div id=\"mod_make\"><div id=\"cmd\">%s</div>",cmd,makefile);
-	// ap_rprintf(r,"<div>Return code:%d</div>",WEXITSTATUS(ret));
-	// ap_rprintf(r,"<pre id=\"output\">%s</pre></div>",make_output);
-
-	// printtable(r, r->headers_in, "Request Headers", "Header", "Value");
-	// printtable(r, r->headers_out, "Response Headers", "Header", "Value");
-	// printtable(r, r->subprocess_env, "Environment", "Variable", "Value");
-
-	// ap_rputs("<div>the_request:",r);	ap_rputs(r->the_request,r);ap_rputs("</div>",r);
-	// ap_rputs("<div>protocol:",r);		ap_rputs(r->protocol,r);ap_rputs("</div>",r);
-	// // ap_rputs("<div>status_line:",r);	ap_rputs(r->status_line,r);ap_rputs("</div>",r);
-	// ap_rputs("<div>method:",r);		ap_rputs(r->method,r);ap_rputs("</div>",r);
-	// ap_rputs("<div>unparsed_uri:",r);	ap_rputs(r->unparsed_uri,r);ap_rputs("</div>",r);
-	// ap_rputs("<div>uri:",r);			ap_rputs(r->uri,r);ap_rputs("</div>",r);
-	// ap_rputs("<div>filename:",r);		ap_rputs(r->filename,r);ap_rputs("</div>",r);
-	// ap_rputs("<div>canonical_filename:",r);ap_rputs(r->canonical_filename,r);ap_rputs("</div>",r);
-	// ap_rputs("<div>path_info:",r);	ap_rputs(r->path_info,r);ap_rputs("</div>",r);
-	// // ap_rputs("<div>args:",r);			ap_rputs(r->args,r);ap_rputs("</div>",r);
     ap_rputs("</body></html>", r);
 		
 	return OK;
@@ -117,24 +68,33 @@ static int make_fixup(request_rec *r) {
 	// 2. The URI does not match the MakeExcludeRegex expression
 		
 	// Locate Makefile: The Makefile should be in SOURCE_ROOT/REL_PATH/Makefile
+	char relpath[MAX_PATH];
 	char makefile[MAX_PATH];
-	strncpy(makefile,SOURCE_ROOT,sizeof(makefile)-1);
-	// Copy the relative part of r->canonical_filename, i.e., the part with the DocumentRoot removed
-	strncat(makefile,r->canonical_filename+strlen(DOCUMENT_ROOT),sizeof(makefile)-strlen(makefile)-1);
+	char make_target[64];
+
+	// Determine the relative path part of r->canonical_filename, i.e., the part with the DocumentRoot removed
+	strncpy(relpath,r->canonical_filename+strlen(DOCUMENT_ROOT),sizeof(relpath)-1);
 	// Truncate it before the basename
-	char* p=strrchr(makefile,'/');
-	if (!p)
-		return DECLINED; // No decipherable make target, give up
-		
-	*p='\0'; // Truncate it
-	
+	char* p=strrchr(relpath,'/');
+	if (p)
+		*++p='\0';
+	else {
+		relpath[0]='/';
+		relpath[1]='\0';
+	}
+
 	// Determine the make target, i.e., the basename of r->canonical_filename
-	char make_target[32];
-	strncpy(make_target,++p,sizeof(make_target)-1);
+	strncpy(make_target,r->canonical_filename+strlen(DOCUMENT_ROOT)+strlen(relpath),sizeof(make_target)-1);
 	make_target[sizeof(make_target)-1]='\0';
-		
+	
+	strncpy(makefile,SOURCE_ROOT,sizeof(makefile)-1);
+	strncat(makefile,relpath,sizeof(makefile)-strlen(makefile)-1);
 	strncat(makefile,MAKEFILE_NAME,sizeof(makefile)-strlen(makefile)-1);
 	makefile[sizeof(makefile)-1]='\0';
+	
+	ap_log_rerror(APLOG_MARK,APLOG_ERR,0,r,"mod_make: relpath:%s",relpath);
+	ap_log_rerror(APLOG_MARK,APLOG_ERR,0,r,"mod_make: makefile:%s",makefile);
+	ap_log_rerror(APLOG_MARK,APLOG_ERR,0,r,"mod_make: make_target:%s",make_target);
 	
 	// If Makefile not found, ignore it (we only care if there •is* a Makefile)
 	struct stat ss;
@@ -143,14 +103,16 @@ static int make_fixup(request_rec *r) {
 	}
 
 	// Build make command
-	char cmd[255];
-	strncpy(cmd,"make -C ",sizeof(cmd)-1);
-	strncat(cmd,(const char*)dirname(makefile),sizeof(cmd)-strlen(cmd)-1);
-	strncat(cmd,MAKE_OPTIONS,sizeof(cmd)-strlen(cmd)-1);
-	strncat(cmd," ",sizeof(cmd)-strlen(cmd)-1);
-	strncat(cmd,make_target,sizeof(cmd)-strlen(cmd)-1);
-	strncat(cmd," 2>&1",sizeof(cmd)-strlen(cmd)-1);
-	cmd[sizeof(cmd)-1]='\0';
+	char* cmd=apr_psprintf(r->pool,"WWWDOCROOT=%s WWWRELPATH=%s %s -f %s -C %s %s %s 2>&1",
+		DOCUMENT_ROOT,
+		relpath,
+		MAKE_PROG,
+		MAKEFILE_NAME,
+		(const char*)dirname(makefile),
+		MAKE_OPTIONS,
+		make_target);
+		
+	ap_log_rerror(APLOG_MARK,APLOG_ERR,0,r,"mod_make: cmd:%s",cmd);
 	
 	// Run Makefile to make target
 	FILE* makep=popen(cmd,"r");
@@ -159,37 +121,55 @@ static int make_fixup(request_rec *r) {
 		return HTTP_INTERNAL_SERVER_ERROR;
 	}
 
+	// Compile regex
+	regex_t regex;
+	// if (regcomp(&regex,"make: \\*\\*\\* No rule to make target `.+'\\. Stop\\.",0)) {
+	if (regcomp(&regex,"No rule to make target",0)) {
+		ap_log_rerror(APLOG_MARK,APLOG_ERR,0,r,"mod_make: regcomp failed");
+		return HTTP_INTERNAL_SERVER_ERROR;
+	}
+	
+	int bSilentFail=FALSE;
+
 	// Read output of make
-	size_t make_output_size=10240;
+	size_t make_output_size=10240; // TODO: allow the buffer to grow beyond 10K
 	char* make_output=apr_palloc(r->pool,make_output_size);
 	make_output[0]='\0';
 	make_output_size--;
 	p=make_output; // reuse p
 	do {
-		char* newp=fgets(p,make_output_size,makep);
+		const char* newp=fgets(p,make_output_size,makep);
 		if (newp) {
 			make_output_size-=strlen(newp);
 			p+=strlen(newp);
+			
+			if (regexec(&regex,newp,0,0,0)==0) {
+				ap_log_rerror(APLOG_MARK,APLOG_ERR,0,r,"mod_make: regex match:%s",newp);
+				bSilentFail=TRUE;
+			}
+
 		}
 	}
 	while (!feof(makep));
 
 	int ret=pclose(makep);
 	
+	ap_log_rerror(APLOG_MARK,APLOG_ERR,0,r,"mod_make: make exit code:%d",WEXITSTATUS(ret));
+	
 	if (WEXITSTATUS(ret)) {// make did not complete successfully, output make's output and tell Apache to stop.
+		// TODO: Look for regex 'make: \*\*\* No rule to make target `[TARGET]'' and if found, silently fail
+		if (bSilentFail) {
+			ap_log_rerror(APLOG_MARK,APLOG_ERR,0,r,"mod_make: silently failing.");
+			return DECLINED;
+		}
+		
 		// Store make_output in request_rec so that the handler can display it 
-		ap_log_rerror(APLOG_MARK,APLOG_ERR,0,r,"mod_make:original table value:%s",make_output);
 		apr_table_set(r->notes,"make_output",make_output);
-
-		const char* tmp=apr_table_get(r->notes,"make_output");
-		ap_log_rerror(APLOG_MARK,APLOG_ERR,0,r,"mod_make:checking table value:%s",tmp);
-
+		// Redirect to our own content handler
 		ap_internal_redirect(MAKEERROR_URI,r);
 		return OK;
 	}
 	
-	// If all ok, return DECLINED so that Apache continues to let the request continue
-
     return DECLINED;
 }
 
